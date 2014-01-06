385d4a8a13522c35703b04d5d17954c144a3d9bb
maxkatz6 <maxkatz6@outlook.com>
2014-01-06 12:50:17 +0200

    update structure

--- Ormeli/App/Program.cs
+++ Ormeli/App/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ormeli
+namespace Ormeli.App
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
