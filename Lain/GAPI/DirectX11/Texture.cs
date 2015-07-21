41f48d0fa4cd1d86629eb2b4b1aa7b4ba08646d4
maxkatz6 <maxkatz6@outlook.com>
2015-07-22 00:09:40 +0300

    add DevIL support. System.Drawing has been removed

--- Lain/GAPI/DirectX11/Texture.cs
+++ Lain/GAPI/DirectX11/Texture.cs
@@ -1,14 +1,11 @@
 ﻿#if DX
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
 using System.Runtime.InteropServices;
 using Lain.Core;
 using SharpDX;
 using SharpDX.Direct3D11;
 using SharpDX.DXGI;
-using Color = SharpDX.Color;
 
 namespace Lain.GAPI
 {
@@ -118,32 +115,38 @@ namespace Lain.GAPI
 			return ResourceView.NativePointer.GetHashCode();
 		}
 
-		private static Texture Load(string fileName)
-		{
-			if (!File.Exists(fileName))
-			{
-				ErrorProvider.SendError("Dx11.Texture.Load() : file " + fileName + " not found.");
-				return Null;
-			}
-			var bit = new Bitmap(fileName);
+        private static Texture Load(string fileName)
+        {
+            int id = 0;
+            DevIL.GenImages(1, ref id);
+            DevIL.BindImage(id);
+            if (!(DevIL.LoadImage(fileName) && DevIL.ConvertImage(DevIL.RGBA, DevIL.FLOAT)))
+            {
+                ErrorProvider.SendError("Dx11.Texture.Load() : "+DevIL.GetError()+" on load " + fileName + ".");
+                return Null;
+            }
+            int w = DevIL.GetInteger(DevIL.IMAGE_WIDTH);
+            int h = DevIL.GetInteger(DevIL.IMAGE_HEIGHT);
 
-			var newMatrix = new Color4[bit.Height, bit.Width];
-			int newColumn, newRow = 0;
-			for (var oldColumn = 0; oldColumn < bit.Height; oldColumn++)
-			{
-				newColumn = 0;
-				for (var oldRow = 0; oldRow < bit.Width; oldRow++)
-				{
-					var p = bit.GetPixel(oldRow, oldColumn);
-					newMatrix[newRow, newColumn] = new Color(p.R, p.G, p.B, p.A);
-					newColumn++;
-				}
-				newRow++;
-			}
-			return Create(newMatrix);
-		}
+            return new Texture(new ShaderResourceView(App.Render.Device, new Texture2D(App.Render.Device, new Texture2DDescription
+                {
+                    ArraySize = 1,
+                    BindFlags = BindFlags.ShaderResource,
+                    CpuAccessFlags = 0,
+                    Format = Format.R32G32B32A32_Float,
+                    Height = h,
+                    MipLevels = 1,
+                    OptionFlags = 0,
+                    Usage = ResourceUsage.Default,
+                    Width = w,
+                    SampleDescription = new SampleDescription(1, 0)
+                }, new[]
+                {
+                    new DataBox(new DataStream(DevIL.GetData(), w*h*16, true, true).DataPointer,w*16, 0)
+                })), w, h);
+        }
 
-		public static unsafe Texture Create(Color4[,] array, Format format = Format.R32G32B32A32_Float)
+        public static unsafe Texture Create(Color4[,] array, Format format = Format.R32G32B32A32_Float)
 		{
 			var w = array.GetLength(1);
 			var h = array.GetLength(0);
@@ -170,7 +173,73 @@ namespace Lain.GAPI
 					SampleDescription = new SampleDescription(1, 0)
 				}, v)), w, h);
 		}
-	}
+
+        private static class DevIL
+        {
+            public const uint IMAGE_WIDTH = 0x0DE4;
+            public const uint IMAGE_HEIGHT = 0x0DE5;
+            public const int RGBA = 0x1908;
+            public const int FLOAT = 0x1406;
+
+            static DevIL()
+            {
+                Init();
+            }
+
+            [DllImport("DevIL.dll", EntryPoint = "ilInit")]
+            public static extern void Init();
+            [DllImport("DevIL.dll", EntryPoint = "ilGenImages")]
+            public static extern void GenImages(int d, ref int id);
+            [DllImport("DevIL.dll", EntryPoint = "ilBindImage")]
+            public static extern void BindImage(int id);
+            [DllImport("DevIL.dll", EntryPoint = "ilDeleteImage")]
+            public static extern void DeleteImage(int id);
+            [DllImport("DevIL.dll", EntryPoint = "ilLoadImage", CharSet = CharSet.Ansi)]
+            public static extern bool LoadImage(string filename);
+            [DllImport("DevIL.dll", EntryPoint = "ilGetInteger")]
+            public static extern int GetInteger(uint enumValue);
+            [DllImport("DevIL.dll", EntryPoint = "ilConvertImage")]
+            public static extern bool ConvertImage(int formatEnum, int typeEnum);
+            [DllImport("DevIL.dll")]
+            public static extern void ilDeleteImages(uint num, ref uint handle);
+            [DllImport("DevIL.dll", EntryPoint = "ilGetError", CallingConvention = CallingConvention.StdCall)]
+            public static extern ErrorType GetError();
+            [DllImport("DevIL.dll", EntryPoint = "ilGetData", CallingConvention = CallingConvention.StdCall)]
+            public static unsafe extern IntPtr GetData();
+            public enum ErrorType
+            {
+                NoError = 0x0000,
+                InvalidEnum = 0x0501,
+                OutOfMemory = 0x0502,
+                FormatNotSupported = 0x0503,
+                InternalError = 0x0504,
+                InvalidValue = 0x0505,
+                IllegalOperation = 0x0506,
+                IllegalFileValue = 0x0507,
+                IllegalFileHeader = 0x0508,
+                InvalidParameter = 0x0509,
+                CouldNotOpenFile = 0x050A,
+                InvalidExtension = 0x050B,
+                FileAlreadyExists = 0x050C,
+                OutFormatSame = 0x050D,
+                StackOverflow = 0x050E,
+                StackUnderflow = 0x050F,
+                InvalidConversion = 0x0510,
+                BadDimensions = 0x0511,
+                FileReadError = 0x0512,
+                FileWriteError = 0x0512,
+                GifError = 0x05E1,
+                JpegError = 0x05E2,
+                PngError = 0x05E3,
+                TiffError = 0x05E4,
+                MngError = 0x05E5,
+                Jp2Error = 0x05E6,              
+                ExrError = 0x05E7,
+                UnknownError = 0x05FF
+            }
+
+        }
+    }
 }
 
 #endif
