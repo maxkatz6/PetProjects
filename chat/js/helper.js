a8414d8b8936a04e074e87449165df8a56fffce1
maxkatz6 <maxkatz6@outlook.com>
2017-08-21 01:48:46 +0300

    Update getWebRTCInfo

--- chat/js/helper.js
+++ chat/js/helper.js
@@ -76,34 +76,51 @@
         return "?start="+(h||m||s?(h?t[0]*3600:0)+(m?t[h?1:0]*60:0)+(s?t[h?(m?2:1):(m?1:0)]*1:0):t);
     },
     getWebRTCInfo: function () {
-        var e, o;
-        window.mozRTCPeerConnection || navigator.mozGetUserMedia ? (e = "moz", o = parseInt(navigator.userAgent.match(/Firefox\/([0-9]+)\./)[1], 10)) : (window.webkitRTCPeerConnection || navigator.webkitGetUserMedia) && (e = "webkit", o = navigator.userAgent.match(/Chrom(e|ium)/) && parseInt(navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./)[2], 10));
-        var t = window.mozRTCPeerConnection || window.webkitRTCPeerConnection,
-            i = window.mozRTCIceCandidate || window.RTCIceCandidate,
-            n = window.mozRTCSessionDescription || window.RTCSessionDescription,
-            r = window.webkitMediaStream || window.MediaStream,
-            a = "https:" === window.location.protocol && ("webkit" === e && o >= 26 || "moz" === e && o >= 33),
-            d = window.AudioContext || window.webkitAudioContext,
-            s = document.createElement("video"),
-            p = s && s.canPlayType && "probably" === s.canPlayType('video/webm; codecs="vp8", vorbis'),
-            w = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.msGetUserMedia || navigator.mozGetUserMedia;
-        return !!t && p && !!w && {
-            prefix: e,
-            browserVersion: o,
-            supportRTCPeerConnection: !!t,
-            supportVp8: p,
-            supportGetUserMedia: !!w,
-            supportDataChannel: !!(t && t.prototype && t.prototype.createDataChannel),
-            supportWebAudio: !(!d || !d.prototype.createMediaStreamSource),
-            supportMediaStream: !(!r || !r.prototype.removeTrack),
-            supportScreenSharing: !!a,
-            audioContext: d,
-            peerConnection: t,
-            sessionDescription: n,
-            iceCandidate: i,
-            mediaStream: r,
-            getUserMedia: w
+        // created by @HenrikJoreteg
+        var prefix;
+        var version;
+
+        if (window.mozRTCPeerConnection || navigator.mozGetUserMedia) {
+            prefix = 'moz';
+            version = parseInt(navigator.userAgent.match(/Firefox\/([0-9]+)\./)[1], 10);
+        } else if (window.webkitRTCPeerConnection || navigator.webkitGetUserMedia) {
+            prefix = 'webkit';
+            version = navigator.userAgent.match(/Chrom(e|ium)/) && parseInt(navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./)[2], 10);
         }
+
+        var PC = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+        var IceCandidate = window.mozRTCIceCandidate || window.RTCIceCandidate;
+        var SessionDescription = window.mozRTCSessionDescription || window.RTCSessionDescription;
+        var MediaStream = window.webkitMediaStream || window.MediaStream;
+        var screenSharing = window.location.protocol === 'https:' &&
+            ((prefix === 'webkit' && version >= 26) ||
+                (prefix === 'moz' && version >= 33))
+        var AudioContext = window.AudioContext || window.webkitAudioContext;
+        var videoEl = document.createElement('video');
+        var supportVp8 = videoEl && videoEl.canPlayType && videoEl.canPlayType('video/webm; codecs="vp8", vorbis') === "probably";
+        var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.msGetUserMedia || navigator.mozGetUserMedia;
+
+        // export support flags and constructors.prototype && PC
+        return {
+            prefix: prefix,
+            browserVersion: version,
+            support: !!PC && !!getUserMedia,
+            // new support style
+            supportRTCPeerConnection: !!PC,
+            supportVp8: supportVp8,
+            supportGetUserMedia: !!getUserMedia,
+            supportDataChannel: !!(PC && PC.prototype && PC.prototype.createDataChannel),
+            supportWebAudio: !!(AudioContext && AudioContext.prototype.createMediaStreamSource),
+            supportMediaStream: !!(MediaStream && MediaStream.prototype.removeTrack),
+            supportScreenSharing: !!screenSharing,
+            // constructors
+            AudioContext: AudioContext,
+            PeerConnection: PC,
+            SessionDescription: SessionDescription,
+            IceCandidate: IceCandidate,
+            MediaStream: MediaStream,
+            getUserMedia: getUserMedia
+        };
     },
     popupCenter:function(url, title, w, h){
         var dualScreenLeft=window.screenLeft!=undefined?window.screenLeft:screen.left;
