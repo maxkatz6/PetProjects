2a93bfc2ad670d1e9bd6f668b9ec4d4c4b827075
maxkatz6 <maxkatz6@outlook.com>
2017-09-23 16:53:17 +0300

    New VER

--- chat/index.php
+++ chat/index.php
@@ -12,7 +12,7 @@ error_reporting(E_ALL);
 
 define('DS', DIRECTORY_SEPARATOR );
 define('SCHAT_PATH', __DIR__);
-define('VER', 'v14');
+define('VER', 'v15');
 
 require(SCHAT_PATH.DS.'lib'.DS.'classes.php');
 
