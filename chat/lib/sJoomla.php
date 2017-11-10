84b9ca1d236280d9d57925295f69df6b46b81f8d
maxkatz6 <maxkatz6@outlook.com>
2017-11-10 10:54:59 +0200

    Update joompla file path

--- chat/lib/sJoomla.php
+++ chat/lib/sJoomla.php
@@ -6,7 +6,7 @@ define('JPATH_BASE', dirname(SCHAT_PATH));
 
 require_once( JPATH_BASE .DS.'includes'.DS.'defines.php' );
 require_once( JPATH_BASE .DS.'includes'.DS.'framework.php' );
-require_once( JPATH_BASE .DS.'libraries'.DS.'joomla'.DS.'factory.php');
+require_once( JPATH_BASE .DS.'libraries'.DS.'joomla'.DS.'database'.DS.'factory.php');
 
 $mainframe = JFactory::getApplication('site');
 $mainframe->initialise();
