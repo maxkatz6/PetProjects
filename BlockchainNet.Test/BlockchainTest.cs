d0f900ab7b350cecd9d58c40633178417a4e138d
maxkatz6 <maxkatz6@outlook.com>
2017-12-12 22:33:55 +0200

    Add blockchain mine test

---
+++ BlockchainNet.Test/BlockchainTest.cs
@@ -0,0 +1,21 @@
+﻿namespace BlockchainNet.Test
+{
+    using BlockchainNet.Core;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BlockchainTest
+    {
+        [TestMethod]
+        public void Blockchain_MineTest()
+        {
+            var blockchain = Blockchain.CreateNew();
+            blockchain.NewTransaction("Alice", "Bob", 100);
+            var block = blockchain.Mine();
+            var isValid = blockchain.IsValidChain(blockchain.Chain);
+
+            Assert.IsTrue(isValid, "Blockchain is invalid");
+        }
+    }
+}
