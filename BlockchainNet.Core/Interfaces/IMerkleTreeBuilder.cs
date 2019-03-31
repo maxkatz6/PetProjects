306f2ead3f54abb93c7ace18865b0c9465dadbb7
maxkatz6 <maxkatz6@outlook.com>
2019-04-01 02:33:24 +0300

    Add IMerkleTreeBuilder

---
+++ BlockchainNet.Core/Interfaces/IMerkleTreeBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace BlockchainNet.Core.Interfaces
+{
+    using System.Collections.Generic;
+
+    using BlockchainNet.Core.Models;
+
+    public interface IMerkleTreeBuilder
+    {
+        MerkleNode BuildTree(IEnumerable<byte[]> nodes);
+    }
+}
