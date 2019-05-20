0236e3d72596adef346d958d93e286b0e1b97f9a
maxkatz6 <maxkatz6@outlook.com>
2019-05-20 23:49:27 +0300

    Add DeferredEventArgs

---
+++ BlockchainNet.View.Shared/EventArgs/DeferredEventArgs.cs
@@ -0,0 +1,16 @@
+﻿namespace BlockchainNet.Shared.EventArgs
+{
+    using System;
+
+    using Nito.AsyncEx;
+
+    public class DeferredEventArgs : EventArgs
+    {
+        internal IDeferralSource DeferralSource { get; set; }
+
+        public IDisposable GetDeferral()
+        {
+            return DeferralSource.GetDeferral();
+        }
+    }
+}
---
+++ BlockchainNet.View.Shared/EventArgs/DeferredEventArgsExtension.cs
@@ -0,0 +1,25 @@
+﻿namespace BlockchainNet.Shared.EventArgs
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Nito.AsyncEx;
+
+    public static class DeferredEventArgsExtension
+    {
+        public static Task InvokeAsync<T>(this EventHandler<T>? eventHandler, object sender, T eventArgs)
+            where T : DeferredEventArgs
+        {
+            if (eventHandler == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            var manager = new DeferralManager();
+
+            eventArgs.DeferralSource = manager.DeferralSource;
+            eventHandler.Invoke(sender, eventArgs);
+            return manager.WaitForDeferralsAsync();
+        }
+    }
+}
