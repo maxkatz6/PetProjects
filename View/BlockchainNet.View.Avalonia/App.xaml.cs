e71a28eee2782e8a4ce27613b2bc5b182a0f5b7e
maxkatz6 <maxkatz6@outlook.com>
2019-06-03 02:54:58 +0300

    Some clean

--- View/BlockchainNet.View.Avalonia/App.xaml.cs
+++ View/BlockchainNet.View.Avalonia/App.xaml.cs
@@ -2,9 +2,36 @@
 {
     using Avalonia;
     using Avalonia.Markup.Xaml;
+    using BlockchainNet.View.Gui.ViewModels;
+    using BlockchainNet.View.Gui.Views;
+    using BlockchainNet.Messenger;
 
     public class App : Application
     {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start(AppMain, args);
+        }
+
+        private static void AppMain(Application app, string[] args)
+        {
+            var window = new MainWindow
+            {
+                DataContext = new MainWindowViewModel(new MessengerServiceLocator()),
+            };
+
+            app.Run(window);
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+        {
+            return AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .With(new X11PlatformOptions { UseGpu = false })
+                .With(new Win32PlatformOptions { UseDeferredRendering = false })
+                .UseReactiveUI();
+        }
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
