bd8327298ff10bc736445b93891833a6499dcc86
maxkatz6 <maxkatz6@outlook.com>
2020-02-14 04:10:21 -0500

    Add BlazorCalculator.ServerRender for debugging

---
+++ BlazorCalculator/BlazorCalculator.ClientRender/Pages/FetchData.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorCalculator.ClientRender.Pages
+{
+    public partial class FetchData : ComponentBase
+    {
+        private WeatherForecast[] forecasts;
+
+        [Inject]
+        protected HttpClient Http { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetJsonAsync<WeatherForecast[]>("https://localhost:5001/sample-data/weather.json");
+        }
+
+        public class WeatherForecast
+        {
+            public DateTime Date { get; set; }
+
+            public int TemperatureC { get; set; }
+
+            public string Summary { get; set; }
+
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+}
