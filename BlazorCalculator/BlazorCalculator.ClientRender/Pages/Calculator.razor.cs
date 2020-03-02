20fdf032775c3db1f9b41c340e88b5eaa41b8911
maxkatz6 <maxkatz6@outlook.com>
2020-03-01 21:35:13 -0500

    Add blazor calculator page with custom logic

---
+++ BlazorCalculator/BlazorCalculator.ClientRender/Pages/Calculator.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorCalculator.ClientRender.Pages
+{
+    public partial class Calculator : ComponentBase
+    {
+        [Parameter]
+        public double NumberA { get; set; }
+
+        [Parameter]
+        public double NumberB { get; set; }
+
+        [Parameter]
+        public double Result { get; set; }
+
+        [Parameter]
+        public CalcOperation Operation { get; set; }
+
+        protected void Recalculate()
+        {
+            Result = Operation switch
+            {
+                CalcOperation.Add => NumberA + NumberB,
+                CalcOperation.Substract => NumberA - NumberB,
+                _ => double.NaN
+            };
+        }
+
+        public enum CalcOperation
+        { 
+            Add,
+            Substract
+        }
+    }
+}
